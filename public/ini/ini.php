8bfbfb334760afe0412487f572c92488f7f41b95
Akash Uphade <31629732+akashuphade@users.noreply.github.com>
2020-06-29 17:01:18 +0530

    Removed the actual credentials with dummy

--- public/ini/ini.php
+++ public/ini/ini.php
@@ -4,16 +4,16 @@
 define('BASE_PATH', realpath(dirname(__FILE__) . '/..'));
 
 // from Email id to filter the inbox
-define('EMAIL_TO_FILTER_FROM', 'uphade.akash25@gmail.com');
+define('EMAIL_TO_FILTER_FROM', 'EMAIL_TO_FILTER_FROM');  // EX :xyx@gmail.com
 
 // folder name to move your deleted mails
-define('TRASH_FOLDER', '[Gmail]/Trash');
+define('TRASH_FOLDER', 'TRASH_FOLDER'); // EX: [Gmail]/Trash
 
 // Email server credentials
-define('SERVER','{imap.gmail.com:993/imap/ssl}');
-define('USER', 'uphade.akash25@gmail.com');
-define('PASSWORD', 'bmkkkjnybxsfbbsd');
-define('FOLDER', 'INBOX');
+define('SERVER','{imap.gmail.com:993/imap/ssl}'); // This is URL for Gmail, change it if you are using other mail server
+define('USER', 'USERNAME'); // Username
+define('PASSWORD', 'PASSWORD'); // Password
+define('FOLDER', 'INBOX'); //Folder from which to fetch the emails
 
 
 /**
